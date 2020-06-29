c872ebd519f15923cc4954a23a09193ccd9815ac
Sarah Zurmühle <s.zurmuehle@hispeed.ch>
2020-06-29 11:52:28 +0200

    add loop in orchestrator and tidy up code

--- orchestrator.py
+++ orchestrator.py
@@ -9,7 +9,7 @@ import random
 from termcolor import colored
 
 initial_data = random.sample(range(10), 10)
-max_time_step = 99
+max_time_step = 9
 min_time_step = 0
 curr_time_step = 0
 
@@ -23,6 +23,9 @@ envB = run_agent('SimulatorB', attributes=dict(data=initial_data))
 
 
 class Orchestrator:
+    """
+    Orchestrator responsible for linking simulators to agents and running them.
+    """
 
     def __init__(self):
 
@@ -31,6 +34,11 @@ class Orchestrator:
         print("Initial data: " + str(self.data))
 
     def run_simulation(self):
+        """
+        connect all simulators to an agent and let them run
+
+        :return:
+        """
         # Create a proxy to SimulatorA and log a message
         agent_simulatorA = ns.proxy('SimulatorA')
         agent_simulatorA.log_info('Hello world! Simulator A')
@@ -39,52 +47,79 @@ class Orchestrator:
         agent_simulatorB = ns.proxy('SimulatorB')
         agent_simulatorB.log_info('Hello world! Simulator B')
 
-        # System configuration
+        # System configuration:
+        # connect agent of simulator A with agent of simulator B and vis versa
         addr_simulatorA = agent_simulatorA.bind('REP', alias='mainA', handler=handler_simulatorB)
         agent_simulatorB.connect(addr_simulatorA, alias='mainA')
         addr_simulatorB = agent_simulatorB.bind('REP', alias='mainB', handler=handler_simulatorA)
         agent_simulatorA.connect(addr_simulatorB, alias='mainB')
 
-        # execute simulator B with input from simulator A
-        simulatorA_input = {
+        # initial simulatorA output
+        simulatorA_output = {
             "time_step": self.time_step,
             "data": self.data
         }
-        agent_simulatorB.send("mainA", json.dumps(simulatorA_input))
-        self.time_step += 1
-        simulatorB_input_data = agent_simulatorB.recv('mainA')
-        print("simulator B input data: " + str(simulatorB_input_data))
 
-        # execute simulator A with input from simulator B
-        simulatorB_input = {
-            "time_step": self.time_step,
-            "data": simulatorB_input_data
-        }
-        agent_simulatorA.send("mainB", json.dumps(simulatorB_input))
-        self.time_step += 1
-        simulatorA_input_data = agent_simulatorA.recv('mainB')
-        print("simulator A input data: " + str(simulatorA_input_data))
+        while min_time_step <= self.time_step < max_time_step:
+            # execute simulator B with output from simulator A
+            simulatorB_output = self.execute_simulator_with_output_from_other_simulator(
+                agent_simulatorB, simulatorA_output, 'mainA')
+            print("SimulatorB output: " + str(simulatorB_output))
+
+            # execute simulator A with output from simulator B
+            simulatorA_output = self.execute_simulator_with_output_from_other_simulator(
+                agent_simulatorA, simulatorB_output, 'mainB')
+            self.time_step += 1
+            print("SimulatorA output: " + str(simulatorA_output))
 
         ns.shutdown()
 
+    def execute_simulator_with_output_from_other_simulator(self, agent_simulator_receiver, simulator_sender_input,
+                                                           agent_sender_name):
+        """
+        Execute a receiver agent with input from a sender simulators agent
+
+        :param agent_simulator_receiver:    simulator to be executed and which receives input
+        :param simulator_sender_input:      input for receiving agent coming from the sending agent
+        :param agent_sender_name:           name of sending agent
+        :return:                            json containing the output data computed with executed simulator
+        """
+        agent_simulator_receiver.send(agent_sender_name, json.dumps(simulator_sender_input))
+        simulator_receiver_output_data = agent_simulator_receiver.recv(agent_sender_name)
+
+        simulator_receiver_output = {
+            "time_step": self.time_step,
+            "data": simulator_receiver_output_data
+        }
+        return simulator_receiver_output
+
 
 def handler_simulatorA(agent, message):
-    try:
-        input_json = json.loads(message)
-        # set new computed data
-        new_output_data = simulatorA.run_time_step(input_json["time_step"], input_json["data"])
-        return new_output_data
-    except JSONDecodeError:
-        print(colored("------------\nwrong input format coming from " + str(agent) + "\ninput: "
-              + str(message) + "\nexpected input format: json\n------------", 'yellow'))
+    simulator_output_data = handler_execution(agent, message, simulatorA)
+    return simulator_output_data
 
 
 def handler_simulatorB(agent, message):
+    simulator_output_data = handler_execution(agent, message, simulatorB)
+    return simulator_output_data
+
+
+def handler_execution(agent, message, simulator):
+    """
+    Agent handler function called whenever an agent uses the send() command. It runs the given simulator with the data
+    extracted from the message.
+
+    :param agent:       agent of the simulator
+    :param message:     json string containing the run information for the simulator. In the form
+                        {"time_step": 0, "data": [0,1,2,...]}
+    :param simulator:   simulator which should run
+    :return:            data output of simulator in the form [3,4,5,...]
+    """
     try:
         input_json = json.loads(message)
         # set new computed data
-        new_output_data = simulatorB.run_time_step(input_json["time_step"], input_json["data"])
-        return new_output_data
+        simulator_output_data = simulator.run_time_step(input_json["time_step"], input_json["data"])
+        return simulator_output_data
     except JSONDecodeError:
         print(colored("------------\nwrong input format coming from " + str(agent) + "\ninput: "
               + str(message) + "\nexpected input format: json\n------------", 'yellow'))
